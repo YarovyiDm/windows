fc5295d644d62f3705ed76c92b1102b743fde04a
YarovyiDM <yarovoy.dmytro@gmail.com>
2026-02-15 02:55:09 +0200

    small fixes

--- src/Containers/TaskBar/Components/Currency/Components/Widget/Widget.helpers.ts
+++ src/Containers/TaskBar/Components/Currency/Components/Widget/Widget.helpers.ts
@@ -1,7 +1,7 @@
 import { MonoCurrency } from "./Widget.types";
 import { currenciesMap } from "./Widget.constants";
 
-export const humanReadable = (monoData: MonoCurrency[]) => {
+export const humanReadable = (monoData: MonoCurrency[] = []) => {
     return monoData.filter(item => item.currencyCodeB === 980 && [840,978,985,826].includes(item.currencyCodeA))
         .map(item => ({
             from: currenciesMap[item.currencyCodeA],
